642027192d1c8c15a0fbd5b3b482e49ef7802351
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-12 22:01:54 +0100

    React Query Devtools

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import { SuperHeroesPage } from "./components/SuperHeroes.page";
 import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
 import { HomePage } from "./components/Home.page";
 import NavBar from "./NavBar";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
 
+
+const queryClient = new QueryClient();
 function App() {
+
   return (
-    <>
+    
+  <QueryClientProvider client={queryClient}>
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/super" element={<SuperHeroesPage />}></Route>
         <Route path="/rq" element={<RQSuperHeroesPage />}></Route>
       </Routes>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
+    </QueryClientProvider>
+   
   );
 }
 
